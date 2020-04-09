254de40d0f52772b809945097789c4da267d772b
nkue-yst <yoshi_frederic@yahoo.co.jp>
2020-04-10 01:10:28 +0900

    Add order of 'O'

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
   
 class Board extends React.Component {
@@ -20,13 +15,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      isX: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.isX ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      isX: !this.state.isX,
+    });
   }
 
   renderSquare(i) {
