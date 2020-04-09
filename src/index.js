459dabdb66435952e95742d2eb8b518a9412ac34
nkue-yst <yoshi_frederic@yahoo.co.jp>
2020-04-10 01:13:19 +0900

    Show the next order.

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ class Board extends React.Component {
   }
   
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.isX ? 'X' : 'O');
 
     return (
       <div>
