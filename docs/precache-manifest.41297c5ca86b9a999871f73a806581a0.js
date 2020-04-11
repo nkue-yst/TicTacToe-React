812762bd88f6a763a0d5191bea31018ab10e84eb
nkue-yst <yoshi_frederic@yahoo.co.jp>
2020-04-11 17:11:01 +0900

    Add github pages.

---
+++ docs/precache-manifest.41297c5ca86b9a999871f73a806581a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4e9abe07824edfc9a89fcd2c614560",
+    "url": "/TicTacToe-React/index.html"
+  },
+  {
+    "revision": "8ae820ef3ce6b67f0201",
+    "url": "/TicTacToe-React/static/css/main.57c2dab8.chunk.css"
+  },
+  {
+    "revision": "f422fff2c076de4f1f98",
+    "url": "/TicTacToe-React/static/js/2.11218696.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe-React/static/js/2.11218696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae820ef3ce6b67f0201",
+    "url": "/TicTacToe-React/static/js/main.d7be4b40.chunk.js"
+  },
+  {
+    "revision": "f69c2366fb6df361c206",
+    "url": "/TicTacToe-React/static/js/runtime-main.731db08c.js"
+  }
+]);
